9168dec2571a189572703a5d86e6099f90cf390d
So Sakamaki <115204648+Sakamaki-So@users.noreply.github.com>
2023-12-03 15:31:53 +0900

    バグの修正
    異なるやることリストのデータで優先度を算出していたのを修正。優先度を決める数値を調整。入力コマンドにより集計を開始するように変更。

--- FirebaseManager.cs
+++ FirebaseManager.cs
@@ -9,10 +9,12 @@ namespace TaskReviewServer
     internal class FirebaseManager
     {
         const string AUTH_DOMAIN = "taskview-f65fb.firebaseapp.com";
+        const string FIREBASE_URL = "https://taskview-f65fb-default-rtdb.firebaseio.com/";
         const string TASK_KEY = "tasks";
         const string EVALUATION_KEY = "evaluations";
         const string PRIORITY_KEY = "priority";
         readonly static string colorBar = new('%', 40);
+        readonly static string colorBar2 = new('/', 40);
 
         internal static async Task<FirebaseClient> GetClient()
         {
@@ -28,8 +30,7 @@ namespace TaskReviewServer
             var auth = new FirebaseAuthClient(config);
             var userCredential = await auth.SignInWithEmailAndPasswordAsync(
                 GetValue("メールアドレスを入力してください："), GetValue("パスワードを入力してください："));
-            var firebase = new FirebaseClient(
-                "https://taskview-f65fb-default-rtdb.firebaseio.com/",
+            var firebase = new FirebaseClient(FIREBASE_URL,
                 new FirebaseOptions
                 {
                     AuthTokenAsyncFactory = () => userCredential.User.GetIdTokenAsync()
@@ -42,50 +43,69 @@ namespace TaskReviewServer
             return Console.ReadLine() ?? string.Empty;
         }
 
-        internal static async System.Threading.Tasks.Task ChangePriority(FirebaseClient firebase)
+        internal static async System.Threading.Tasks.Task ChangePriority(FirebaseClient firebase, bool isHide)
         {
+            Console.ForegroundColor = ConsoleColor.Cyan;
             Console.WriteLine("TODOリストのデータをFirebaseから取得しています");
+            if (isHide == false) Console.WriteLine($"\n{colorBar2}");
+            Console.ResetColor();
             var tasks = await firebase.Child(TASK_KEY).OnceAsync<FirebaseData.Task>();
             foreach (var task in tasks)
             {
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine(colorBar);
-                Console.ResetColor();
-                task.Object.Print(task.Key);
-                var priority = await CalcPriorityFromEvaluation(firebase, task.Key);
+                if (isHide == false)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(colorBar);
+                    Console.ResetColor();
+                    task.Object.Print(task.Key);
+                }
+                var priority = await CalcPriorityFromEvaluation(firebase, task.Key, isHide);
                 await firebase.Child(TASK_KEY).Child(task.Key).Child(PRIORITY_KEY).PutAsync(priority);
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine($"{colorBar}\n\n\n");
+                if (isHide == false)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"{colorBar}\n\n\n");
+                    Console.ResetColor();
+                }
+            }
+            if (isHide == false)
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine(colorBar2);
                 Console.ResetColor();
             }
         }
-        private static async Task<double> CalcPriorityFromEvaluation(FirebaseClient firebase, string key)
+        private static async Task<double> CalcPriorityFromEvaluation(FirebaseClient firebase, string key, bool isHide)
         {
             double score = 0;
+            int evalNum = 0;
             var evals = await firebase
                 .Child(EVALUATION_KEY)
-                //.OrderBy(FIREBASE_KEY_NAME_KEY)
                 .OnceAsync<Evaluation>();
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine("\n<評価データ>");
-            Console.ResetColor();
+            if (isHide == false)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("\n<評価データ>");
+                Console.ResetColor();
+            }
             foreach (var eval in evals)
             {
-                if (eval.Object.firebaseKey == key) continue;
-                eval.Object.Print(eval.Key);
-                if (eval.Object.isFinished != false) score += 2.5;
+                if (eval.Object.firebaseKey != key) continue;
+                if (isHide == false) eval.Object.Print(eval.Key);
+                evalNum++;
+                if (eval.Object.isFinished == false) score += 4.0;
                 else score += eval.Object.evaluation switch
                 {
-                    0 => 2.0,
-                    1 => 1.5,
-                    2 => 1.0,
-                    3 => 0.5,
-                    4 => 0.0,
+                    0 => 3.0,
+                    1 => 2.5,
+                    2 => 2.0,
+                    3 => 1.0,
+                    4 => 0.5,
                     5 => -0.5,
-                    _ => 0.0
+                    _ => 1.0
                 };
             }
-            return Math.Clamp(score / evals.Count, 0.0, 2.0);
+            return Math.Clamp(evalNum == 0 ? score : score / evalNum, 0.0, 2.0);
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -4,20 +4,103 @@ namespace TaskReviewServer
 {
     internal class Application
     {
-        static int TIME = 1000 * 60; // ミリ秒指定
+        static readonly int TIME_MINUTE = 1000 * 60; // ミリ秒指定
+        const string LAUNCH_CMD = "launch";
+        const string HIDE_OPTION = "--hide";
+        const string TIME_OPTION = "-t";
+        const string EXIT_CMD = "exit";
         private static async Task Main(string[] args)
         {
             var firebase = await FirebaseManager.GetClient();
             Console.Clear();
-            Launch(firebase);
+            while(true)
+            {
+                var command = getCommandValue(getCommand());
+                if (!command.Item1)
+                {
+                    Console.WriteLine("コマンドが無効です");
+                    continue;
+                }
+                if (command.Item2 == 0) break;
+                Launch(firebase, command.Item2, command.Item3);
+            }
         }
 
-        private static void Launch(FirebaseClient firebase)
+        private static void Launch(FirebaseClient firebase, int minute, bool isHide)
         {
+            Console.Clear();
+            Console.WriteLine($"{minute} : {isHide}");
             Timer timer = new Timer(
-                async o => await FirebaseManager.ChangePriority(firebase), null, 0, TIME);
+                async o => await FirebaseManager.ChangePriority(firebase, isHide), null, 0, TIME_MINUTE * minute);
             Console.ReadLine();
             timer.Dispose();
+        }
+        // isValidCmd, minute, isHide
+        private static (bool, int, bool) getCommandValue(string command)
+        {
+            var isHide = false;
+            var minute = 1;
+            var commandArray = command.Split(" ");
+            if(commandArray.Length == 0) return (false, minute, isHide);
+            if(commandArray.Length == 1)
+            {
+                if (commandArray[0] == LAUNCH_CMD) return (true, minute, isHide);
+                if (commandArray[0] == EXIT_CMD) return (true, 0, isHide);
+            }
+            if (commandArray[0] != LAUNCH_CMD) return (false, minute, isHide);
+            if(commandArray.Length == 2)
+            {
+                if (commandArray[1] == HIDE_OPTION) return (true, minute, true);
+            }
+            if(commandArray.Length == 3)
+            {
+                if (commandArray[1] == TIME_OPTION)
+                {
+                    if (int.TryParse(commandArray[2], out minute))
+                    {
+                        if(0 < minute && minute < 14400) return (true, minute, isHide);
+                    }
+                }
+            }
+            if(commandArray.Length == 4)
+            {
+                if(commandArray[1] == TIME_OPTION)
+                {
+                    if (int.TryParse(commandArray[2], out minute))
+                    {
+                        if (0 < minute && minute < 14400)
+                        {
+                            if (commandArray[3] == HIDE_OPTION) return (true, minute, true);
+                            Console.WriteLine("オプション引数 <minute> が無効か大きすぎます");
+                        }
+                    }
+                }
+                if (commandArray[1] == HIDE_OPTION)
+                {
+                    if (commandArray[2] == TIME_OPTION)
+                    {
+                        if (int.TryParse(commandArray[3], out minute))
+                        {
+                            if (0 < minute && minute < 14400) return (true, minute, true);
+                        }
+                        else Console.WriteLine("オプション引数 <minute> が無効か大きすぎます");
+                    }
+                }
+            }
+            return (false, minute, isHide);
+        }
+
+        private static string getCommand()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("使いかた:");
+            Console.WriteLine($"{LAUNCH_CMD} < option >: 集計開始");
+            Console.WriteLine($"{EXIT_CMD}: 終了");
+            Console.WriteLine($"< option >: {TIME_OPTION} < minute >: < minute >分ごとに集計");
+            Console.WriteLine($"            {HIDE_OPTION}       : 集計情報を非表示");
+            Console.ResetColor();
+            Console.Write(">");
+            return Console.ReadLine() ?? string.Empty;
         }
     }
 }
